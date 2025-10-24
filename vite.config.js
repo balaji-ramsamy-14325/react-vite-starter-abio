9c1d66b594a63724eead799d82286254a1b8aeb9
balaji-ramsamy-14325 <balaji.ramsamy@zohocorp.com>
2025-10-24 20:01:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "frame-ancestors 'none';"
+    }
+  }
 })
