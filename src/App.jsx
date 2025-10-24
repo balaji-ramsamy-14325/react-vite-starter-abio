dbef8c5b28e4c3c2f0b91daa5cbaf0cb34fb2e93
balaji-14325 <balaji.ramsamy@zohocorp.com>
2025-10-24 20:17:50 +0530

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <iframe frameborder=0 width="800" height="600" src="https://analytics.zoho.in/open-view/465351000000010712/901af83f3907bd68fbd7c531f1201b7b"></iframe>
+        <iframe frameBorder="0" width="800" height="600" src="https://analytics.zoho.in/open-view/465351000000010712/901af83f3907bd68fbd7c531f1201b7b"></iframe>
       </div>
     </>
   )
