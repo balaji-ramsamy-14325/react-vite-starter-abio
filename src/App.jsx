8623ac38993b8eeea13e3e5706e893a50c5d21d1
balaji-ramsamy-14325 <balaji.ramsamy@zohocorp.com>
2025-10-24 20:14:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,6 @@ function App() {
       <div>
         <iframe frameborder=0 width="800" height="600" src="https://analytics.zoho.in/open-view/465351000000010712/901af83f3907bd68fbd7c531f1201b7b"></iframe>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
